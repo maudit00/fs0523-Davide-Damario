d87454297b3a743d5d7ed885baa1af53be9b626f
Maudit00 <davide9311@gmail.com>
2023-11-06 16:35:58 +0100

    first commit

---
+++ m2/s3/g1/script.js
@@ -0,0 +1,16 @@
+class User {
+    constructor (fn, ln, a, l){
+    this.firstName = fn;
+    this.lastName = ln;
+    this.age = a;
+    this.location = l;
+   }
+ageCheck (){
+
+}
+
+}
+
+let p1 = new User ("Davide", `D'Amario`, 30, "Milano")
+
+console.log(p1);
