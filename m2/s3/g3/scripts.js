1b87945228a2670d6dfb06e1b0aa154a73892fab
Maudit00 <davide9311@gmail.com>
2023-11-08 18:08:48 +0100

    main exercise done

--- m2/s3/g3/scripts.js
+++ m2/s3/g3/scripts.js
@@ -1,67 +1,31 @@
-const url =  "https://striveschool-api.herokuapp.com/books";
-let booksArray = [];
+const url = "https://striveschool-api.herokuapp.com/books";
+
 
 fetch(url)
-.then(res => res.json())
-.then(data => {
-    data.forEach(book => {
-    booksArray.push(new Book(book.id, book.category, book.img,book.price, book.title));
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((book) => {
+    
+      let clone = cardClone();
+      let target = document.getElementById("target");
+
+      let image = clone.querySelector(".card-img-top");
+      console.log(image);
+      let title = clone.querySelector(".card-title");
+      let price = clone.querySelector(".card-text");
+
+      image.src = book.img;
+      title.innerText = book.title;
+      price.innerText = `${book.price} €`;
+
+      target.append(clone);
     });
-    console.log(booksArray);
-    booksArray.forEach(book => {
-        new BookCard("#app.book-grid").cardMaker(book);
-    });
-})  
-
-class BookCard {
-    constructor(appAreaSelector,) {
-        this.appArea = document.querySelector(appAreaSelector);
-        this.cardWrapper = null;
-        this.cardImg = null;
-        this.cardBody = null;
-        this.cardTitle = null;
-        this.cardTabContentArea = null;
-        this.scartaButton = null;
-        this.HTMLinit();
-        this.cardClassSetter();
-
-    }
-
-    HTMLinit() {
-        this.cardWrapper = document.createElement("div");
-        this.cardImg = document.createElement("img");
-        this.cardBody = document.createElement("div");
-        this.cardTitle = document.createElement("h5");
-        this.cardTabContent = document.createElement("div");
-        this.scartaButton = document.createElement("button");
-        this.appArea.append(this.cardWrapper,this.cardImg,this.cardBody,this.cardTitle,this.cardTabContent,this.scartaButton);
-    }
-
-    cardClassSetter() {
-        this.cardWrapper.classList.add("card");
-        this.cardWrapper.classList.add("col");
-        this.cardImg.classList.add("card-img-top");
-        this.cardImg.classList.add("img-fluid");
-        this.cardBody.classList.add("card-body");
-        this.cardTitle.classList.add("card-title");
-        this.cardTabContent.classList.add("card-text");
-        this.scartaButton.classList.add("btn");
-        this.scartaButton.classList.add("btn-primary");
-    }
-
-    cardContentSetter() {
+  });
 
 
-}
 
-new BookCard("#app .book-grid");
 
-class Book {
-    constructor(id, category, img, price, title){
-        this.id = id;
-        this.category = category;
-        this.img = img;
-        this.price = price;
-        this.title = title;
-    }
+function cardClone() {
+  let temp = document.getElementsByTagName("template")[0];
+  return temp.content.cloneNode(true);
 }
