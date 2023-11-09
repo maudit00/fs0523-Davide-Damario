ac8311159b5c71f1f81c0efbfbac0fc2d7f73cd1
Maudit00 <davide9311@gmail.com>
2023-11-09 15:57:58 +0100

    id assigned to each photos

--- m2/s3/g4/script.js
+++ m2/s3/g4/script.js
@@ -1,3 +1,4 @@
+const url = "https://api.pexels.com/v1/search?query=";
 const queryUrl = "https://api.pexels.com/v1/search?query=Mountain";
 const queryUrl2 = "https://api.pexels.com/v1/search?query=Pizza";
 const apiKey = "FIYcS3vrRpfihtMR0JEEE4xMHgje6Q6mMqGR5czOo8rCil03yVmh07ti";
@@ -5,6 +6,8 @@ const loadButton = document.querySelector("#load-button");
 const loadButton2 = document.querySelector("#load-button2");
 const editButtons = document.querySelectorAll(".card-button-2")
 const imgIdArea = document.querySelectorAll(".img-id");
+const cardImg = document.querySelectorAll(".card-img");
+
 
 
 async function loadImages() {
@@ -14,14 +17,16 @@ async function loadImages() {
         }
     })
     let data = await res.json();
-    console.log(data)
 
-    const cardImg = document.querySelectorAll(".card-img");
-    console.log(cardImg);
+
+    imgIdArea.forEach((id, j)=> {
+        id.innerText = data.photos[j].id;
+    });
+
     cardImg.forEach((img, i)=> {
         img.src = data.photos[i].src.original;
     })
-    changeElementContent (imgIdArea, data.id);
+    
 }
 
 async function loadImages2() {
@@ -31,25 +36,45 @@ async function loadImages2() {
         }
     })
     let data = await res.json();
-    console.log(data.photos[0].src.original);
 
-    const cardImg = document.querySelectorAll(".card-img");
-    console.log(cardImg);
+    const cardImg = document.querySelectorAll(".card-img");  
+    imgIdArea.forEach((id, j)=> {
+        id.innerText = data.photos[j].id;
+    });
+
     cardImg.forEach((img, i)=> {
         img.src = data.photos[i].src.original
     })
-    changeElementContent (imgIdArea, data.id);
+
 }
 
+async function loadImagesCustom() {
+    let res = await fetch(url + "dogs", {
+        headers: {
+            "Authorization": `${apiKey}`
+        }
+    })
+    let data = await res.json();
+
+     
+    imgIdArea.forEach((id, j)=> {
+        id.innerText = data.photos[j].id;
+    });
+
+    cardImg.forEach((img, i)=> {
+        img.src = data.photos[i].src.original
+    })
+
+}
 loadButton.addEventListener("click", loadImages);
-loadButton2.addEventListener("click", loadImages2);
+loadButton2.addEventListener("click", loadImagesCustom);
 
   
-changeElementContent (editButtons, "Hide");
-changeElementContent (imgIdArea, "ciao");
+changeElementsContent (editButtons, "Hide");
 
-function changeElementContent (buttons, content) {
-    buttons.forEach((button) => {
-        button.innerHTML = content;
+
+function changeElementsContent (elements, content) {
+    elements.forEach((e) => {
+        e.innerHTML = content;
     })
 }
