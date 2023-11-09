4ea768eb4ee221dc77e14635e19bf9e99175135f
Maudit00 <davide9311@gmail.com>
2023-11-09 16:28:46 +0100

    hide button working query search working

--- m2/s3/g4/script.js
+++ m2/s3/g4/script.js
@@ -4,9 +4,12 @@ const queryUrl2 = "https://api.pexels.com/v1/search?query=Pizza";
 const apiKey = "FIYcS3vrRpfihtMR0JEEE4xMHgje6Q6mMqGR5czOo8rCil03yVmh07ti";
 const loadButton = document.querySelector("#load-button");
 const loadButton2 = document.querySelector("#load-button2");
-const editButtons = document.querySelectorAll(".card-button-2")
+const searchButton = document.querySelector("#search-button");
+const hideButtons = document.querySelectorAll(".card-button-2")
 const imgIdArea = document.querySelectorAll(".img-id");
 const cardImg = document.querySelectorAll(".card-img");
+const userQuery = document.querySelector("#user-query");
+console.log(userQuery);
 
 
 
@@ -49,12 +52,13 @@ async function loadImages2() {
 }
 
 async function loadImagesCustom() {
-    let res = await fetch(url + "dogs", {
+    let res = await fetch(url +userQuery.value, {
         headers: {
-            "Authorization": `${apiKey}`
+            "Authorization":`${apiKey}`
         }
     })
     let data = await res.json();
+    console.log(data);
 
      
     imgIdArea.forEach((id, j)=> {
@@ -66,15 +70,28 @@ async function loadImagesCustom() {
     })
 
 }
+
 loadButton.addEventListener("click", loadImages);
-loadButton2.addEventListener("click", loadImagesCustom);
+loadButton2.addEventListener("click", loadImages2);
+searchButton.addEventListener("click", loadImagesCustom);
 
   
-changeElementsContent (editButtons, "Hide");
+changeElementsContent (hideButtons, "Hide");
+
+hideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let cardArea = button.parentElement.parentElement.parentElement.parentElement
+        hideCard(cardArea);
+    });
+});
 
 
 function changeElementsContent (elements, content) {
     elements.forEach((e) => {
         e.innerHTML = content;
     })
+}
+
+function hideCard (card) {
+    card.remove();
 }
