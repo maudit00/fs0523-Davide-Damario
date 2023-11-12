af5751fc602f09e0beae7dc0c463e73a79163193
Maudit00 <davide9311@gmail.com>
2023-11-12 15:43:36 +0100

    spinner working edit mode working

--- m2/s3/progetto/assets/js/product-details.js
+++ m2/s3/progetto/assets/js/product-details.js
@@ -3,6 +3,7 @@ let id = url.get("id");
 const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZjcwNDk1ZDRmNjAwMTg1NjI0ZDgiLCJpYXQiOjE2OTk2MDgzMjQsImV4cCI6MTcwMDgxNzkyNH0.AjDfv8LqVS2wCnPKoO24OBFxpUsOl6p1MEr0YSvxpyc";
 const API_URL = "https://striveschool-api.herokuapp.com/api/product/";
+let authentication = localStorage.getItem("token");
 
 
 /**** get singolo prodotto */
--- m2/s3/progetto/assets/js/script.js
+++ m2/s3/progetto/assets/js/script.js
@@ -1,20 +1,24 @@
 const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZjcwNDk1ZDRmNjAwMTg1NjI0ZDgiLCJpYXQiOjE2OTk2MDgzMjQsImV4cCI6MTcwMDgxNzkyNH0.AjDfv8LqVS2wCnPKoO24OBFxpUsOl6p1MEr0YSvxpyc";
 const API_URL = "https://striveschool-api.herokuapp.com/api/product/";
+const spinner = document.getElementById("spinner");
+const editModeButton = document.querySelector("#show-edit-buttons");
 
 /*** GET FETCH */
 async function getProducts() {
+  spinner.removeAttribute("hidden");
   return await fetch(API_URL, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${API_KEY}`,
     },
-  }).then((res) => res.json());
+  }).then((res) => res.json())
 }
 
 /**** funzione di rendering prodotti */
 async function renderProducts() {
   let products = await getProducts();
+  spinner.setAttribute("hidden", "");
   let target = document.querySelector(".product-container");
 
   products.forEach((p) => {
@@ -24,8 +28,8 @@ async function renderProducts() {
     let brand = clone.querySelector(".product-brand");
     let desc = clone.querySelector(".product-desc");
     let price = clone.querySelector(".product-price");
-    let moreButton = clone.querySelector("#more");
-    let editButton = clone.querySelector("#edit");
+    let moreButton = clone.querySelector(".more");
+
 
     
     img.src = p.imageUrl;
@@ -48,4 +52,22 @@ renderProducts();
 function cardClone() {
   let temp = document.getElementsByTagName("template")[0];
   return temp.content.cloneNode(true);
+}
+/***** gestione del click della modalità  */
+
+editModeButton.addEventListener("click", () => {
+  showEditButtons();
+let editSpan = document.querySelector("#edit-mode-span");
+let viewSpan = document.querySelector("#view-mode-span");
+editSpan.classList.toggle("d-none");
+viewSpan.classList.toggle("d-none");
+
+});
+
+/***** funzione toggle modalità home page */
+function showEditButtons () {
+  let editButtons = document.querySelectorAll(".edit");
+  editButtons.forEach((b) => {
+    b.classList.toggle("d-none");
+  });
 }
