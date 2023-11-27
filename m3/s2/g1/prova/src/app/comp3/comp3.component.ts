746b2ea0f0ca8729db93fc096fe5f44f362c439d
Maudit00 <davide9311@gmail.com>
2023-11-27 13:12:20 +0100

    commit esercizio fatto ,

---
+++ m3/s2/g1/prova/src/app/comp3/comp3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.comp3',
+  templateUrl: './comp3.component.html',
+  styleUrl: './comp3.component.scss'
+})
+export class Comp3Component {
+
+}
