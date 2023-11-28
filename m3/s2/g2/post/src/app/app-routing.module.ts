df5728458173f719a498ca793a6d1212ae6522ae
Maudit00 <davide9311@gmail.com>
2023-11-28 16:02:48 +0100

    interface, header, and service setted

--- m3/s2/g2/post/src/app/app-routing.module.ts
+++ m3/s2/g2/post/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PostAttiviComponent } from './pages/post-attivi/post-attivi.component';
+import { PostInattiviComponent } from './pages/post-inattivi/post-inattivi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'post-attivi',
+    component: PostAttiviComponent
+  },
+  {
+    path: 'post-inattivi',
+    component: PostInattiviComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- m3/s2/g2/post/src/app/app.module.ts
+++ m3/s2/g2/post/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PostAttiviComponent } from './pages/post-attivi/post-attivi.component';
+import { PostInattiviComponent } from './pages/post-inattivi/post-inattivi.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavUlComponent } from './component/header/nav-ul/nav-ul.component';
+import { ButtonsWrapperComponent } from './component/header/buttons-wrapper/buttons-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostAttiviComponent,
+    PostInattiviComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavUlComponent,
+    ButtonsWrapperComponent
   ],
   imports: [
     BrowserModule,
