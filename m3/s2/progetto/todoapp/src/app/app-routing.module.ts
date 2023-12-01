d0db012edbad2eb79692722c8d45a55e5bf6e5f1
Maudit00 <davide9311@gmail.com>
2023-12-01 10:10:27 +0100

    routing making

--- m3/s2/progetto/todoapp/src/app/app-routing.module.ts
+++ m3/s2/progetto/todoapp/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoComponent } from './pages/todo/todo.component';
+import { CompletedComponent } from './pages/completed/completed.component';
+import { Page404Component } from './pages/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  },
+  {
+    path: 'completed',
+    component: CompletedComponent
+  },
+  {
+    path: '**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- m3/s2/progetto/todoapp/src/app/app.module.ts
+++ m3/s2/progetto/todoapp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoComponent } from './pages/todo/todo.component';
+import { CompletedComponent } from './pages/completed/completed.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodoComponent,
+    CompletedComponent,
+    HeaderComponent,
+    FooterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
