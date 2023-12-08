4a1ed28baaaecd751b45f1ec8ff7d377d69e01d2
Maudit00 <davide9311@gmail.com>
2023-12-08 19:33:43 +0100

    login working trying to handle errors

--- m3/s3/progetto/meteo-app/src/app/pages/auth/auth.service.ts
+++ m3/s3/progetto/meteo-app/src/app/pages/auth/auth.service.ts
@@ -3,8 +3,10 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment.development';
 import { IAuthData } from '../../Models/i-auth-data';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, tap } from 'rxjs';
 import { IRegister } from '../../Models/i-register';
+import { ILogin } from '../../Models/i-login';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 
 @Injectable({
@@ -17,7 +19,7 @@ export class AuthService {
 
   registerUrl:string = `${environment.userUrl}/register`;
   loginUrl:string = `${environment.userUrl}/login`;
-
+  jwt:JwtHelperService = new JwtHelperService();
   authSub = new BehaviorSubject<IAuthData | null>(null);
 
   user$ = this.authSub.asObservable();
@@ -26,6 +28,66 @@ export class AuthService {
 
   register(user:IRegister):Observable<IAuthData> {
     return this.http.post<IAuthData>(this.registerUrl, user);
+  }
+
+  login(data:ILogin):Observable<IAuthData> {
+  return this.http.post<IAuthData>(this.loginUrl, data)
+  .pipe(tap(data =>{
+    this.authSub.next(data);
+    localStorage.setItem('token', JSON.stringify(data))
+  }
+  ), catchError(this.errors));
+  }
+
+  handleErrors (){
+    return (error:any) => {
+      console.log(error);
+      return Observable.throw(error);
+    }
+  }
+  autoLogOut (jwtString:string) {
+    const expDate = this.jwt.getTokenExpirationDate(jwtString) as Date;
+    const expMs = expDate.getTime() - new Date().getTime();
+
+    setTimeout(() => {
+      this.logout();
+    }, expMs);
+  }
+
+  logout() {
+    this.authSub.next(null);
+    localStorage.removeItem('token');
+    this.route.navigate(['/login']);
+  }
+
+  restoreSession (){
+    const userJson: string | null = localStorage.getItem('token');
+    if (!userJson) return
+    const authData: IAuthData = JSON.parse(userJson);
+    if (this.jwt.isTokenExpired(authData.accessToken)) return
+
+    this.authSub.next(authData);
+    this.autoLogOut(authData.accessToken);
+  }
+
+  errors(err: any) {
+    switch (err.error) {
+        case "Email and Password are required":
+            return new Error('Email e password obbligatorie');
+            break;
+        case "Email already exists":
+            return new Error('Utente esistente');
+            break;
+        case 'Email format is invalid':
+            return new Error('Email scritta male');
+            break;
+        case 'Cannot find user':
+            return new Error('utente inesistente');
+            break;
+            default:
+        return new Error('Errore');
+            break;
+    }
   }
 
 
--- m3/s3/progetto/meteo-app/src/app/pages/auth/login/login/login.component.ts
+++ m3/s3/progetto/meteo-app/src/app/pages/auth/login/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { ILogin } from '../../../../Models/i-login';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private authSvc: AuthService, private route: Router) {}
 
+  @ViewChild ('f', { static: true })
+
+  form!:NgForm;
+  regex: RegExp = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/;
+
+  login() {
+    this.authSvc.login(this.form.value).subscribe((res) => {
+      this.route.navigate(['/home']);
+    });
+  }
+
+  isEmail(input: NgModel): boolean {
+    return this.regex.test(input.value);
+  }
 }
