ad67bde79a9baf80ce320488f5c72ede5ab802fd
Maudit00 <davide9311@gmail.com>
2023-12-09 19:13:18 +0100

    tabs working search page on working

--- m3/s3/progetto/meteo-app/src/app/pages/dashboard/dashboard.module.ts
+++ m3/s3/progetto/meteo-app/src/app/pages/dashboard/dashboard.module.ts
@@ -5,18 +5,26 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { TabgroupComponent } from './tabgroup/tabgroup.component';
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     TabgroupComponent,
+    SearchComponent,
+    ProfileComponent,
+    FavoritesComponent,
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    FormsModule
   ]
 })
 export class DashboardModule { }
---
+++ m3/s3/progetto/meteo-app/src/app/pages/dashboard/search/search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {
+input:string="";
+
+
+}
