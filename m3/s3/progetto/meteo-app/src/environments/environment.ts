cbe12c5e0b79e278fcd0f112bd5e3eaf3878e76d
Maudit00 <davide9311@gmail.com>
2023-12-11 03:34:57 +0100

    environment set

--- m3/s3/progetto/meteo-app/src/environments/environment.ts
+++ m3/s3/progetto/meteo-app/src/environments/environment.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  weatherUrl:" api.openweathermap.org/data/2.5/forecast?&appid=85bb4f8f3961b22c474f2fc5c2ec36e9?units=metrics"
+  userUrl:"http://localhost:3000",
+  favoriteUrl:"http://localhost:3000/favorites",
+  fiveday:"http://api.openweathermap.org/data/2.5/forecast?",
+  actual:"https://api.openweathermap.org/data/2.5/weather?",
+  geoUrl :"http://api.openweathermap.org/geo/1.0/direct?q=",
+  key:"&appid=85bb4f8f3961b22c474f2fc5c2ec36e9",
+  iconsUrl:'https://openweathermap.org/img/wn/'
 };
