d2c5f4124e6e99af60552292ff1e6f31df31d420
Maudit00 <davide9311@gmail.com>
2023-12-11 03:20:01 +0100

    footer done profile done

--- m3/s3/progetto/meteo-app/src/app/pages/dashboard/profile/profile.component.ts
+++ m3/s3/progetto/meteo-app/src/app/pages/dashboard/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { IUser } from '../../../Models/i-user';
+import { Observable, map } from 'rxjs';
+import { IAuthData } from '../../../Models/i-auth-data';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+constructor(private authSvc:AuthService) {
+this.authSvc.user$.subscribe(user => user ? this.user = user : this.user = null)
+}
 
+  user:IAuthData | null = null;
+
+  logout() {
+    this.authSvc.logout();
+  }
 }
